8ac403867adda6dd62a9263ecf87d6067c52d17a
Shawnnr1475 <radebeshawnn@gmail.com>
2022-09-27 17:20:07 +0200

    register client route

--- routes/clientroutes.js
+++ routes/clientroutes.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt =  require("bcrypt")
 const clientSchema = require("../models/clientmodel")
 
 router.get("/",(req,res)=>{
     res.json("Hello world")
 })
 
-router.get("/clients",async(req,res)=>{
-    clientSchema.find({},(err,docs)=>{
+router.get("/client",async(req,res)=>{
+    clientSchema.find({email:req.body.email},(err,docs)=>{
         if(!err){
             res.json(docs)
         }
@@ -18,4 +19,26 @@ router.get("/clients",async(req,res)=>{
     
 })
 
+router.post("/client/register", async(req,res)=>{
+    let password = ""
+    await bcrypt.hash(req.body.password, 10).then(function(hash) {
+        password = hash
+    });
+    new clientSchema({
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        cell: req.body.cell,
+        address: req.body.address,
+        coordinates: req.body.coordinates,
+        password: password
+    }).save((err,docs)=>{
+        if(!err){
+            res.json(docs)
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
 module.exports = router
