5565ac750a614c8b99699ef7ca1aae8d92994832
Shawnnr1475 <radebeshawnn@gmail.com>
2022-10-04 18:15:21 +0200

    setup client routes

--- routes/clientroutes.js
+++ routes/clientroutes.js
@@ -19,26 +19,48 @@ router.get("/client",async(req,res)=>{
     
 })
 
+router.post("/client/signin" , async(req,res)=>{
+    try{
+        clientSchema.findOne({email:req.body.email},(err,docs)=>{
+            if(!err){
+                bcrypt.compare(req.body.password, docs.password, function(err, result) {
+                    if(result){
+                        res.json(docs)
+                    }
+                    else{
+                        res.json({message:"Incorrect Password"})
+                    }
+                });
+            }
+            else if(docs === {}){
+                res.json({message:"Not found"})
+                res.status = 200
+            }
+        })
+    }catch(err){
+        console.log(err)
+    }
+})
+
 router.post("/client/register", async(req,res)=>{
-    // let password = ""
-    // await bcrypt.hash(req.body.password, 10).then(function(hash) {
-    //     password = hash
-    // });
-    new clientSchema({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        cell: req.body.cell,
-        address: req.body.address,
-        coordinates: req.body.coordinates,
-        password: req.body.password
-    }).save((err,docs)=>{
-        if(!err){
-            res.json(docs)
-        }
-        else{
-            console.log(err)
-        }
-    })
+    await bcrypt.hash(req.body.password,10).then((hash)=>{
+        const newClient = new clientSchema({
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            cell: req.body.cell,
+            address: req.body.address,
+            coordinates: req.body.coordinates,
+            password: hash
+        }).save((err,docs)=>{
+            if(!err){
+                res.json(docs)
+            }
+            else{
+                console.log(err)
+            }
+        })
+    });
+
 })
 module.exports = router
